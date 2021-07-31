4625da06006dd0a827eae201bf03a65034c27d49
Guucat <1027804480@qq.com>
2021-07-31 22:45:40 +0800

    "完成时钟类"

--- MyCode/src/week_1/task_3/MyClock.java
+++ MyCode/src/week_1/task_3/MyClock.java
@@ -1,9 +1,26 @@
 package week_1.task_3;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * @author shengyi
  * @create 2021/7/31 - 21:55
  */
 public class MyClock {
+    public void setTimeByHand(int year, int month, int day) {
+        while (true) {
+            LocalDate localDate = LocalDate.of(year, month, day);
+            System.out.println(localDate);
+        }
+    }
+    public  void setTimeAutomatically() {
+        while (true) {
+            LocalDate localDate = LocalDate.now();
+            System.out.println(localDate);
+        }
 
+    }
 }
---
+++ MyCode/src/week_1/task_3/RunMyRock.java
@@ -0,0 +1,15 @@
+package week_1.task_3;
+
+/**
+ * @author shengyi
+ * @create 2021/7/31 - 22:07
+ */
+public class RunMyRock {
+    public static void main(String[] args) {
+        MyClock myClock = new MyClock();
+        //获取程序运行时间
+        //myClock.setTimeAutomatically();
+        //手动设置时间
+        myClock.setTimeByHand(2020,12,4);
+    }
+}
