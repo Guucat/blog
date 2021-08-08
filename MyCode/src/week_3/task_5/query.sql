0b020eb10030645771d27a1c93a8ef9757d3ea04
Guucat <1027804480@qq.com>
2021-08-08 21:14:37 +0800

    "增加SQL语句 "

--- MyCode/src/week_3/task_5/query.sql
+++ MyCode/src/week_3/task_5/query.sql
@@ -1,8 +1,8 @@
 #查询各科成绩最高和最低的分,以如下的形式显示:课程号,最高分,最低分
-SELECT course_id FROM course;
-SELECT score FROM () ,
+SELECT course_id AS "课程号", MAX(score) AS "最高分", MIN(score) AS "最低分" FROM score GROUP BY course_id;
 
 #查询学生的总成绩并进行排名
-
+SELECT student_id, SUM(score) FROM score GROUP BY student_id ORDER BY SUM(score) DESC;
 
 #查询平均成绩大于60分的学生的学号和平均成绩
+SELECT student_id, AVG(score) FROM score  GROUP BY student_id HAVING AVG(score) > 60 ORDER BY AVG(score) DESC;
